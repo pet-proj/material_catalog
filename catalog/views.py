60ff77a9da327258afa34d3e1af5165445f4d9fd
Yehor Shramchenko <yegor.shramchenko+git@gmail.com>
2026-05-30 11:41:08 -0700

    Initial Search/Filtering implemetned. Filter from ALL products if any filters persist

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Product, Category, Tag
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    query = request.GET.get("q", "")
+    selected_category = request.GET.get("category", "")
+    selected_tags = request.GET.getlist("tags")
+
+    if query:
+        products = products.filter(description__icontains=query) | products.filter(
+            name__icontains=query
+        )
+
+    if selected_category:
+        products = products.filter(category_id=selected_category)
+
+    if selected_tags:
+        products = products.filter(tags__in=selected_tags).distinct()
+
+    context = {
+        "products": products,
+        "categories": categories,
+        "tags": tags,
+        "query": query,
+        "selected_category": selected_category,
+        "selected_tags": selected_tags,
+    }
+
+    return render(request, "catalog/product_list.html", context)
